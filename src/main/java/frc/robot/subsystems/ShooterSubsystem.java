5945cadaffa023185e7a3e9c9c79ebb616bfd360
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2022-02-03 09:17:03 -0800

    base shooter sub code

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.CanIdConstants;
+
+public class ShooterSubsystem extends SubsystemBase{
+    private final TalonSRX shooter = new TalonSRX(CanIdConstants.SHOOTER_MASTER_ID);
+
+    public ShooterSubsystem(){
+        shooter.configOpenloopRamp(1);
+        shooter.setNeutralMode(NeutralMode.Brake);
+    }
+
+    public void setShooterVelo(double velo){
+        shooter.set(ControlMode.PercentOutput, velo);
+    }
+}
