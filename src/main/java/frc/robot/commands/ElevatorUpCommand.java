eb9ec89d7fe171b520b1533dee1c4e2bbe381fff
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2022-02-03 11:56:43 -0800

    base elevator up cmd code

---
+++ src/main/java/frc/robot/commands/ElevatorUpCommand.java
@@ -0,0 +1,24 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.ElevatorSubsystem;
+
+public class ElevatorUpCommand extends CommandBase{
+    ElevatorSubsystem elevatorSubsystem;
+
+    public ElevatorUpCommand(ElevatorSubsystem elevatorSubsystem) {
+        this.elevatorSubsystem = elevatorSubsystem;
+        addRequirements(elevatorSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        elevatorSubsystem.setElevatorVelo(0.55);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        elevatorSubsystem.setElevatorVelo(0);
+    }
+}
