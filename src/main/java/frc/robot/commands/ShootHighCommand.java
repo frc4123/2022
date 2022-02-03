15982afe6458fc95e997def26a9b3f4d9bda561b
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2022-02-03 14:36:41 -0800

    base shooter code for high goal

---
+++ src/main/java/frc/robot/commands/ShootHighCommand.java
@@ -0,0 +1,24 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShootHighCommand extends CommandBase{
+    ShooterSubsystem shooterSubsystem;
+
+    public ShootHighCommand(ShooterSubsystem shooterSubsystem) {
+        this.shooterSubsystem = shooterSubsystem;
+        addRequirements(shooterSubsystem);
+    }
+    
+    @Override
+    public void execute() {
+        shooterSubsystem.setShooterVelo(0.8);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooterSubsystem.setShooterVelo(0);
+    }
+}
