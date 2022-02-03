2175e7379e90a95eda23ce490b09e4db2ff772ce
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2022-02-03 11:56:20 -0800

    base elevator system code

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.CanIdConstants;
+
+public class ElevatorSubsystem extends SubsystemBase{
+    private final TalonSRX elev = new TalonSRX(CanIdConstants.ELEVATOR_MASTER_ID);
+
+    public ElevatorSubsystem(){
+        elev.configOpenloopRamp(1);
+        elev.setNeutralMode(NeutralMode.Brake);
+    }
+
+    public void setElevatorVelo(double velo){
+        elev.set(ControlMode.PercentOutput, velo);
+    }
+}
