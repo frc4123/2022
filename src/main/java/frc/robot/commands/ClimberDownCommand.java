e69792dad2f7eb8ad3fe094f5e6f5fc621eaa0e9
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2022-02-07 12:03:58 -0800

    base climber down cmd code

---
+++ src/main/java/frc/robot/commands/ClimberDownCommand.java
@@ -0,0 +1,24 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.ClimberSubsystem;
+
+public class ClimberDownCommand extends CommandBase{
+    ClimberSubsystem climberSubsystem;
+
+    public ClimberDownCommand(ClimberSubsystem climberSubsystem){
+        this.climberSubsystem = climberSubsystem;
+        addRequirements(climberSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        climberSubsystem.setClimberVelo(-0.4);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        climberSubsystem.setClimberVelo(0);
+    }
+}
