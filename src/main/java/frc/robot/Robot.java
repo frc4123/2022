214b9d7160d51b68b5ad68b4cf1a4911c4bf7332
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2022-03-02 09:46:35 -0800

    ahaha clearStickyFault no worky

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj.PowerDistribution;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -29,10 +28,6 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    clearStickyFaults();
-  }
-
-  public void clearStickyFaults() {}
 
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
