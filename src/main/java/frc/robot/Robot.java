a416ae1f43ab44e0b63273759b30b654215a9d0f
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2022-03-02 09:48:32 -0800

    my dumbass forgot a bracket

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,6 +28,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+  }
 
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
