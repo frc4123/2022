166c622ade63de50e9f9e056fa5aa48fc71fab0b
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2022-03-13 21:19:03 -0700

    possible low goal far shot

--- src/main/java/frc/robot/commands/ShootHighCommand.java
+++ src/main/java/frc/robot/commands/ShootHighCommand.java
@@ -1,24 +1,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-/**
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.networktables.NetworkTableEntry;
-*/
 
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShootHighCommand extends CommandBase{
     ShooterSubsystem shooterSubsystem;
-    /**
-    private ShuffleboardTab tab = Shuffleboard.getTab("Main");
-    public NetworkTableEntry maxSpeed = 
-    tab.add("Shooter High Speed", 1)
-        .withWidget(BuiltInWidgets.kTextView)
-        .getEntry();
-    */
 
     public ShootHighCommand(ShooterSubsystem shooterSubsystem) {
         this.shooterSubsystem = shooterSubsystem;
@@ -27,11 +14,7 @@ public class ShootHighCommand extends CommandBase{
     
     @Override
     public void execute() {
-        /**
-        double max = maxSpeed.getDouble(0.2125);
-        shooterSubsystem.setShooterVelo(1 * max);
-        */
-        shooterSubsystem.setShooterVelo(0.5);
+        shooterSubsystem.setShooterVelo(0.4);
     }
 
     @Override
