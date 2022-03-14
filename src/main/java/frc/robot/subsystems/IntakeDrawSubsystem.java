5214f9e70bc69de23178ca0a61fb9d28536b773a
DaSausiBoi <61775079+DaSausiBoi@users.noreply.github.com>
2022-03-13 21:22:52 -0700

    removed some comments

--- src/main/java/frc/robot/subsystems/IntakeDrawSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeDrawSubsystem.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -13,20 +9,13 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CanIdConstants;
 
 public class IntakeDrawSubsystem extends SubsystemBase {
-
     private final TalonSRX motor = new TalonSRX(CanIdConstants.INTAKE_DRAW_ID);
 
-    /** Creates a new IntakeDrop. */
     public IntakeDrawSubsystem() {
         motor.configOpenloopRamp(1);
         motor.setNeutralMode(NeutralMode.Brake);
     }
 
-    /**
-     * Sets the velocity of the Intake motor.
-     * 
-     * @param velo the velocity of the intake motor in percent
-     */
     public void setIntakeDrawVelo(double velo) {
         motor.set(ControlMode.PercentOutput, velo);
     }
